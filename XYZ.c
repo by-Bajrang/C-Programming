9729dc7a5b845dcca7710c607991e437b05bb316
by-Bajrang <bybajrangyadav@gmail.com>
2023-07-12 23:04:19 +0530

    updated

---
+++ XYZ.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    float a,b;
+    printf("Enter the value of a\n");
+    scanf("%f",&a);
+
+    printf("Enter the value of b\n");
+    scanf("%f",&b);
+
+    printf("divide the a and b is %f\n",a/b);
+
+    return 0;
+}
